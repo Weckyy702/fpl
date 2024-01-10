61dfcc2175db29a9da28df2c8a97f3f7393f82fd
Weckyy702 <50154363+Weckyy702@users.noreply.github.com>
2024-01-10 19:46:20 +0100

    Token: implement Display trait for Tokens
    This makes error messages much nicer

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Write};
+
 use crate::Identifier;
 
 #[derive(Debug)]
@@ -9,4 +11,19 @@ pub enum Token {
     Semi,
     Number(f64),
     Identifier(Identifier),
+}
+
+impl Display for Token {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        use Token::*;
+        match self {
+            Colon => f.write_char(':'),
+            ThinArrow => f.write_str("=>"),
+            LParen => f.write_char('('),
+            RParen => f.write_char(')'),
+            Semi => f.write_char(';'),
+            Number(x) => write!(f, "{x}"),
+            Identifier(name) => write!(f, "{name}"),
+        }
+    }
 }
